523a6077e0ae25e067b21842bcc0570fe531a844
Lucas Flatter <flatterlr@gmail.com>
2013-03-13 21:36:31 -0400

    first

---
+++ README.txt
@@ -0,0 +1,6 @@
+Used this path animation jquery module: https://github.com/weepy/jquery.path
+
+to create a radial menu with css circles akin to a menu I liked on the Path iPhone app. I'm hoping to create  module so it's easier to create these types of menus in the future. Right now, things are mostly hard coded, besides the number of menu items being dynamic. I also need to get the first item to appear in the correct position, and mess with styling a lot to get the content centered in the circle list items. 
+
+Here's the live version: 
+http://lucasflatter.com/practice/radialMenu/
---
+++ javascript/main2.js
@@ -0,0 +1,104 @@
+$(document).ready(function(){
+	
+	makeRadial($('.radialMenu'), 50, 150); 
+
+}); 
+
+function makeRadial(listQuery, itemSize, radius){
+	
+	var $radialMenu = listQuery; 
+	var $menuItem = $($radialMenu).find('li'); 
+	
+	var size = itemSize; 
+	var BorderRadius = size/2; 
+	
+	$($radialMenu).css('position','relative'); 
+	$($menuItem).css('list-style-type', 'none')
+				.css('display', 'block')
+				  .css('width', ""+size)
+				  .css('height', ""+size)
+				  .css('border-radius',""+BorderRadius+"px")
+				  .css('position','absolute')
+				  .css('left','0px')
+				  .css('top','0px'); 
+	
+	var i = 0; 
+	$.each($($menuItem), function() {
+	
+		$(this).addClass('menuItem'); 
+		
+		if(i > 0)
+		{
+			$(this).css('top' ,'0px');
+		}
+		else
+		{
+			$(this).css('z-index','200')
+				   .addClass('headItem');
+				   
+		}
+		i++; 
+	}); 
+	
+	$.each($('.menuItem a'), function() {
+		$(this).css('position','absolute'); 
+		$(this).css('top',''+ BorderRadius -$(this).height()/2 + 'px'); 
+		$(this).css('left',''+ BorderRadius - $(this).width()/2 + 'px');  
+	}); 
+	
+	
+	var angle = (360/ (i-1)); 
+	i = 0; 
+	var clicked = false; 
+	$('.headItem a').click(function(event){
+		
+		event.preventDefault(); 
+		$.each($('.menuItem'), function(event) {
+			if(!$(this).hasClass('headItem') && !clicked)
+				{
+				var bezier_params = {
+					start: { 
+					  x: 0, 
+					  y: 0, 
+					  angle: 300
+					},  
+					end: { 
+					  x: radius * Math.cos(i *angle*(Math.PI /180)),
+					  y: radius * Math.sin(i*angle*(Math.PI /180)), 
+					  angle: -550, 
+					  length: 0.55
+					}
+				 }
+				
+				$(this).animate({path : new $.path.bezier(bezier_params)},500);
+				i++; 
+			}
+			
+			else if(!$(this).hasClass('headItem') && clicked)
+			{
+				var bezier_params = {
+					start: { 
+					  x: radius * Math.cos(i *angle*(Math.PI /180)),
+					  y: radius * Math.sin(i*angle*(Math.PI /180)), 
+					  angle: 800
+					},  
+					end: { 
+					  x: 0,
+					  y: 0, 
+					  angle: -550, 
+					  length: 0.55
+					}
+				 }
+				
+				$(this).animate({path : new $.path.bezier(bezier_params)},500);
+				i++; 
+			}
+			
+		});
+		
+		if(clicked){clicked = false; }
+		else clicked = true; 
+	}); 
+	i= 0; 
+	
+}
